520246dc6337e8be989a7cd2464511234d8dfe55
JoseAlvarado13 <jalvaradomatamoros@outlook.com>
2025-01-14 20:39:40 -0600

    [AM-001] I added new comments

--- DataAccess/Commons/SysErrorLogDA.cs
+++ DataAccess/Commons/SysErrorLogDA.cs
@@ -18,6 +18,8 @@ namespace DataAccess.Commons
         /// AM-001
         /// Author: José Andrés Alvarado Matamoros 
         /// Method to save the system error log inside data base.  
+        /// 
+        /// To do: there are an error loading the  dracarys context, I have to review this code in this moment
         /// </summary>
         /// <param name="exceptionDTO">Here came the exception information about error.</param> 
         public int Save(IExceptionDTO exceptionDTO)
